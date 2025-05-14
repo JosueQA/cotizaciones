ffc8d65fd7dcf3025734230a834c2c3526e164b0
JosueQA <josuelml28@hotmail.com>
2025-05-13 23:53:04 -0500

    Avance en Tkinter

--- interfaz/gui/nueva_cotizacion/nueva_cotizacion_paso_2.py
+++ interfaz/gui/nueva_cotizacion/nueva_cotizacion_paso_2.py
@@ -20,13 +20,13 @@ def nueva_cotizacion_paso_2(root):
     fr_grid_0 = new_frame.newFrame(fr_nueva_cotizacion_paso_2.frame)
     fr_grid_0.aparecer()
 
+    '''INGRESAR DATOS'''
     fr_grid_0.generar_label("ITEM", pack_grid_place='grid', bd=3, relief="groove", row=0, column=0, padx=0)
     fr_grid_0.generar_label("CANTIDAD", pack_grid_place='grid', bd=3, relief="groove", row=0, column=1, padx=0)
     fr_grid_0.generar_label("DESCRIPCION", pack_grid_place='grid', bd=3, relief="groove", row=0, column=2, ipadx=30, padx=0)
     fr_grid_0.generar_label("VALOR UNIT.", pack_grid_place='grid', bd=3, relief="groove", row=0, column=3, padx=0)
     fr_grid_0.generar_label("IMPORTE", pack_grid_place='grid', bd=3, relief="groove", row=0, column=4, ipadx=10, padx=0)
     
-    '''INGRESAR DATOS'''
 
     
 
